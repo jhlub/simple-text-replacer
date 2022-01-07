a210315ce31a677e6193304e85172f63212daf18
Jakub Hlubinski <jakub.hlubinski@gmail.com>
2022-01-07 09:48:21 +0100

    refactor: update files naming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 import { Layout } from '../components/Layout';
+import { SEO } from '../components/Seo';
 
-import SEO from '../components/Seo';
 import { TextReplacerComplex } from '../components/text-replacer/TextReplacerComplex';
 
 const IndexPage: React.FC = () => (
